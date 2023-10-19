8b9b5176a27bb9556fa5772e36be0d4fa764d83b
Kirill Martynkov <kmartynkov@yandex-team.ru>
2023-10-19 11:35:28 +0300

    add org-bullets

--- cbbbrother-theme/org-mode-theme.el
+++ cbbbrother-theme/org-mode-theme.el
@@ -2,5 +2,11 @@
 ;;; Commentary:
 ;;; Code:
 
+(setq org-hide-emphasis-markers t)
+
+(use-package org-bullets
+    :config
+    (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
 (provide 'org-mode-theme)
 ;;; org-mode-theme.el ends here
