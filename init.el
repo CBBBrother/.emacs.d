6190b539af31b6c27eed2f272aea75c5254175b4
Kirill Martynkov <kmartynkov@yandex-team.ru>
2023-10-25 13:34:25 +0300

    add vertico, consult and mrginalia

--- init.el
+++ init.el
@@ -74,7 +74,7 @@
  '(jdee-db-spec-breakpoint-face-colors (cons "#f0f0f0" "#928374"))
  '(objed-cursor-color "#9d0006")
  '(package-selected-packages
-   '(yasnippet treemacs-persp treemacs-magit magit org-bullets treemacs-projectile all-the-icons counsel which-key try use-package))
+   '(consult yasnippet treemacs-persp treemacs-magit magit org-bullets treemacs-projectile all-the-icons counsel which-key try use-package))
  '(pdf-view-midnight-colors (cons "#282828" "#fbf1c7"))
  '(rustic-ansi-faces
    ["#fbf1c7" "#9d0006" "#79740e" "#b57614" "#076678" "#b16286" "#427b58" "#282828"])
